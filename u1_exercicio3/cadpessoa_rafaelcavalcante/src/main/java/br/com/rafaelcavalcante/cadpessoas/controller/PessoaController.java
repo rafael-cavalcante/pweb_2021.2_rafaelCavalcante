c76ebd28ad5012ee1ad41ca1787b83eb41eed9bb
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2023-03-04 13:48:27 -0300

    Remover e Alterar Pessoa

--- u1_exercicio3/cadpessoa_rafaelcavalcante/src/main/java/br/com/rafaelcavalcante/cadpessoas/controller/PessoaController.java
+++ u1_exercicio3/cadpessoa_rafaelcavalcante/src/main/java/br/com/rafaelcavalcante/cadpessoas/controller/PessoaController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,21 +17,21 @@ import br.com.rafaelcavalcante.cadpessoas.repository.PessoaRepository;
 @Controller
 @RequestMapping("/")
 public class PessoaController {
-    
+
     @Autowired
     private PessoaRepository pessoaRepo;
 
-    public PessoaController(PessoaRepository pessoaRepo){
+    public PessoaController(PessoaRepository pessoaRepo) {
         this.pessoaRepo = pessoaRepo;
     }
 
     @GetMapping
-    public String index(){
+    public String index() {
         return "index.html";
     }
 
     @GetMapping("/listarPessoas")
-    public ModelAndView listarPessoas(){
+    public ModelAndView listarPessoas() {
         List<Pessoa> pessoas = this.pessoaRepo.findAll();
         ModelAndView mav = new ModelAndView("listarPessoas");
         mav.addObject("pessoas", pessoas);
@@ -37,14 +39,37 @@ public class PessoaController {
     }
 
     @GetMapping("/adicionarPessoa")
-    public ModelAndView formAdicionarPessoa(){
+    public ModelAndView formAdicionarPessoa() {
         ModelAndView mav = new ModelAndView("adicionarPessoa");
         mav.addObject(new Pessoa());
         return mav;
     }
 
     @PostMapping("/adicionarPessoa")
-    public String adicionarPessoa(Pessoa pessoa){
+    public String adicionarPessoa(Pessoa pessoa) {
+        this.pessoaRepo.save(pessoa);
+        return "redirect:/listarPessoas";
+    }
+
+    @GetMapping("/remover/{id}")
+    public ModelAndView removerPessoa(@PathVariable("id") Long id) {
+        Pessoa pessoa = this.pessoaRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("ID Inválido " + id));
+        this.pessoaRepo.delete(pessoa);
+        return new ModelAndView("redirect:/listarPessoas");
+    }
+
+    @GetMapping("/editar/{id}")
+    public ModelAndView editarPessoa(@PathVariable("id") Long id) {
+        Pessoa pessoa = this.pessoaRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("ID Inválido " + id));
+        ModelAndView mav = new ModelAndView("editarPessoa");
+        mav.addObject(pessoa);
+        return mav;
+    }
+
+    @PostMapping("/editar/{id}")
+    public String editarPessoa(@PathVariable("id") Long id, Pessoa pessoa) {
         this.pessoaRepo.save(pessoa);
         return "redirect:/listarPessoas";
     }
--- u1_exercicio3/cadpessoa_rafaelcavalcante/src/main/java/br/com/rafaelcavalcante/cadpessoas/repository/PessoaRepository.java
+++ u1_exercicio3/cadpessoa_rafaelcavalcante/src/main/java/br/com/rafaelcavalcante/cadpessoas/repository/PessoaRepository.java
@@ -1,11 +1,9 @@
 package br.com.rafaelcavalcante.cadpessoas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import br.com.rafaelcavalcante.cadpessoas.model.Pessoa;
 
-@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
 }
