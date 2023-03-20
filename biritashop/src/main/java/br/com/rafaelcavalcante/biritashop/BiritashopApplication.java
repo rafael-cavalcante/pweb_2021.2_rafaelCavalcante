e8a88a9691d950d9f049d37b3709622489973bc9
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2023-03-19 21:36:40 -0300

    Projeto Base Biritashop

---
+++ biritashop/src/main/java/br/com/rafaelcavalcante/biritashop/BiritashopApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelcavalcante.biritashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiritashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiritashopApplication.class, args);
+	}
+
+}
---
+++ biritashop/src/test/java/br/com/rafaelcavalcante/biritashop/BiritashopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelcavalcante.biritashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiritashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
