c1a137b15658ac15eb1cf47188da8f5fbd9fdba8
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2023-03-24 18:23:31 -0300

    Modificações no Listar Dependentes

--- biritashop/src/main/java/br/com/rafaelcavalcante/biritashop/controller/DependenteController.java
+++ biritashop/src/main/java/br/com/rafaelcavalcante/biritashop/controller/DependenteController.java
@@ -3,11 +3,13 @@ package br.com.rafaelcavalcante.biritashop.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.rafaelcavalcante.biritashop.model.Cliente;
@@ -25,13 +27,32 @@ public class DependenteController {
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @GetMapping("/listar/{clienteId}")
-    public ModelAndView listarDependentes(@PathVariable("clienteId") Long clienteId) {
+    /*@GetMapping("/listar")
+    public ModelAndView listarDependentes() {
+        List<Cliente> clientes = this.clienteRepository.findAll();
+        ModelAndView mav = new ModelAndView("/dependente/listarDependentes");
+        mav.addObject("clientes", clientes);
+        return mav;
+    }*/
+
+    @GetMapping("/listar")
+    public ModelAndView listarDependentes(@RequestParam(value = "clienteId", required = false) Long clienteId) {
+        List<Cliente> clientes = this.clienteRepository.findAll();
+        List<Dependente> dependentes = this.dependenteRepository.findByCliente_Id(clienteId);
+        ModelAndView mav = new ModelAndView("/dependente/listarDependentes");
+        mav.addObject("clientes", clientes);
+        mav.addObject("clienteId", clienteId);
+        mav.addObject("dependentes", dependentes);
+        return mav;
+    }
+
+    /*@GetMapping("/listar/{clienteId}")
+    public ModelAndView listarDependentesCliente(@PathVariable("clienteId") Long clienteId) {
         List<Dependente> dependentes = this.dependenteRepository.findByCliente_Id(clienteId);
         ModelAndView mav = new ModelAndView("/dependente/listarDependentes");
         mav.addObject("dependentes", dependentes);
         return mav;
-    }
+    }*/
 
     @GetMapping("/adicionar")
     public ModelAndView formAdicionarDependente() {
