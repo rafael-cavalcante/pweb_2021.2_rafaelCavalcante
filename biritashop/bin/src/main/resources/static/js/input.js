9693dc01ad4f77b2e453dd242d098156a535c0d9
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2023-04-04 12:24:55 -0300

    Revert "Adicionar pedido Com Alterações"
    This reverts commit e7c1e74d2d977056099d3ca8cbf53313f5da9356.

--- biritashop/bin/src/main/resources/static/js/input.js
+++
@@ -1,7 +0,0 @@
-$("input").on('keypress', function (e) {
-    var inputValue = e.key;
-    var regex = (this.value.length === 0) ? /[1-9]/ : /[0-9]/;
-    if (!regex.test(inputValue)) {
-        e.preventDefault();
-    }
-});
--- u1_exercicio3/cadpessoa_rafaelcavalcante/src/main/java/br/com/rafaelcavalcante/cadpessoas/repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.rafaelcavalcante.cadpessoas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.rafaelcavalcante.cadpessoas.model.Pessoa;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-
-}
