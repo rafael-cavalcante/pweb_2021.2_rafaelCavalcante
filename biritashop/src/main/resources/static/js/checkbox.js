32be740708530b1ea2fa8f4ab92522409976b667
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2023-04-11 15:34:18 -0300

    Modificando Funções JavaScript

--- biritashop/src/main/resources/static/js/checkbox.js
+++ biritashop/src/main/resources/static/js/checkbox.js
@@ -1,45 +1,41 @@
-// Get references to the checkboxes
-var masterCheckbox = document.getElementById("master-checkbox");
-var childCheckboxes = document.getElementsByClassName("child-checkbox");
-
-// Add an event listener to the master checkbox
-masterCheckbox.addEventListener("click", function () {
-    // Loop through all child checkboxes and toggle their state
-    for (var i = 0; i < childCheckboxes.length; i++) {
-        childCheckboxes[i].checked = masterCheckbox.checked;
-    }
-});
-
-// Add event listeners to the child checkboxes
-for (var i = 0; i < childCheckboxes.length; i++) {
-    childCheckboxes[i].addEventListener("click", function () {
-        // Check if all child checkboxes are checked
-        var allChecked = true;
-        for (var j = 0; j < childCheckboxes.length; j++) {
-            if (!childCheckboxes[j].checked) {
-                allChecked = false;
-                break;
-            }
-        }
-        // Set the state of the master checkbox
-        masterCheckbox.checked = allChecked;
-    });
-}
-
 function verificarCheckBox() {
-    let checkboxes = document.querySelectorAll('input[type=checkbox]');
-    let selecionado = false;
+    const checkboxes = document.querySelectorAll("input[type=checkbox]");
+    var selecionado = false;
 
-    for (let i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked) {
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked){
             selecionado = true;
             break;
         }
     }
-
-    if (!selecionado) {
-        alert("Selecione pelo menos uma produto.");
-        return false;
-    }
-    return true;
+    return selecionado;
 }
+
+function checkAll(checkBoxAll) {
+    const checkboxes = document.querySelectorAll(".child-checkbox");
+
+    checkboxes.forEach(c => {
+        c.checked = checkBoxAll.checked;
+    });
+}
+
+function check() {
+    const checkboxes = document.querySelectorAll(".child-checkbox");
+    const masterCheckbox = document.getElementById("master-checkbox");
+
+    checkboxes.forEach(c => {
+        c.addEventListener("click", function () {
+            var allChecked = true;
+
+            for (var j = 0; j < checkboxes.length; j++) {
+                if (!checkboxes[j].checked) {
+                    allChecked = false;
+                    break;
+                }
+            }
+            masterCheckbox.checked = allChecked;
+        });
+    });
+}
+
+check();
