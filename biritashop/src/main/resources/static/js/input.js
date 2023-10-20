19c27a6fc0dddff348049c65b8d4c286a28a054f
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2023-10-20 17:59:51 -0300

    Correções no carrinho itens

--- biritashop/src/main/resources/static/js/input.js
+++ biritashop/src/main/resources/static/js/input.js
@@ -14,37 +14,78 @@ $(".input-unidades").on('blur', function () {
     }
 });
 
-$(document).ready(function () {
-    // Função para calcular o valor total
-    function calcularTotal() {
-        var subTotal = 0;
-        
-        // Loop através dos elementos da lista
-        $('.list-group-item').each(function () {
-            var $checkbox = $(this).find('.child-checkbox');
-            var $input = $(this).find('.input-unidades');
-            var $span = $(this).find('.text-preco');
+function calcularTotal() {
+    var subTotal = 0;
 
-            if ($checkbox.is(':checked')) {
-                var quantidade = parseFloat($input.val());
-                var preco = parseFloat($span.text().replace('R$', '').trim());
-                subTotal += quantidade * preco;
-            }
-        });
+    // Loop através dos elementos da lista
+    $('.list-group-item').each(function () {
+        var $checkbox = $(this).find('.child-checkbox');
+        var $input = $(this).find('.input-unidades');
+        var $span = $(this).find('.text-preco');
 
-        var imposto = subTotal * 0.1375;
+        if ($checkbox.is(':checked')) {
+            var quantidade = parseFloat($input.val());
+            var preco = parseFloat($span.text().replace('R$', '').trim());
+            subTotal += quantidade * preco;
+        }
+    });
 
-        var total = subTotal + imposto;
+    var imposto = subTotal * 0.1375;
 
-        // Atualiza o valor da tag strong
-        $('#subTotal').text('R$ ' + subTotal.toFixed(2));
-        $('#imposto').text('R$ ' + imposto.toFixed(2));
-        $('#total').text('R$ ' + total.toFixed(2));
-    }
+    var total = subTotal + imposto;
 
-    // Associe o evento de mudança da checkbox e input number à função de cálculo
-    $('.child-checkbox, .input-unidades, #master-checkbox').on('change', calcularTotal);
+    // Atualiza o valor da tag strong
+    $('#subTotal').text('R$ ' + subTotal.toFixed(2));
+    $('#imposto').text('R$ ' + imposto.toFixed(2));
+    $('#total').text('R$ ' + total.toFixed(2));
+}
 
-    // Inicialize o cálculo quando a página é carregada
-    calcularTotal();
+// Associe o evento de mudança da checkbox e input number à função de cálculo
+$('.child-checkbox, .input-unidades, #master-checkbox').on('change', calcularTotal);
+
+// Inicialize o cálculo quando a página é carregada
+calcularTotal();
+
+const decrementButtons = document.querySelectorAll(".decrement");
+const incrementButtons = document.querySelectorAll(".increment");
+const valueInputs = document.querySelectorAll(".value");
+
+decrementButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        const currentValue = parseInt(valueInputs[index].value);
+        if (currentValue > 1) {
+            valueInputs[index].value = currentValue - 1;
+            calcularTotal();
+        }
+    });
 });
+
+incrementButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        const currentValue = parseInt(valueInputs[index].value);
+        valueInputs[index].value = currentValue + 1;
+        calcularTotal();
+    });
+});
+
+$(function() {
+    $("#formaPagamento").change(function() {
+      var formaPagamento = $(this).val();
+  
+      switch (formaPagamento) {
+        case "dinheiro":
+          $("#div-valor").show();
+          $("#div-numeroCartao").hide();
+          break;
+        case "pix":
+          $("#div-valor").show();
+          $("#div-numeroCartao").hide();
+          break;
+        case "cartao":
+          $("#div-valor").show();
+          $("#div-numeroCartao").show();
+          break;
+      }
+    });
+  });
+  
